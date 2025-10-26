f49ce8bdc77b5ed76cf60c551b05810d853c7777
Irfan-Ali-Dev <yousafbuilders.it@gmail.com>
2025-10-26 07:14:44 +0500

    10 Question solved

--- app.js
+++ app.js
@@ -1,1 +1,72 @@
 console.log("Welcome to Array");
+
+// Solution # 1
+let studentName = ["Irfan", "Imran", "Ali"]
+console.log("Solution # 1 " + studentName);
+studentName.push("Hussain")
+console.log("Solution # 1.2 " + studentName);
+
+
+// Solution # 2
+let fruitName = ["Mango","Orange","Banana"]
+console.log("Solution # 2 " + fruitName);
+fruitName.pop()
+console.log("Solution # 2.2 " + fruitName);
+
+// Solution # 3
+let weekName = ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+console.log("Solution # 3 " + weekName);
+weekName.unshift("Monday")
+console.log("Solution # 3.2 " + weekName);
+
+// Solution # 4
+let color = ["Yellow", " Green", " Pink", " Black"]
+console.log("Solution # 4 " + color);
+color.shift()
+console.log("Solution # 4.2 " + color);
+
+// Solution # 5
+let subjectsName = ["Pak Study", " Islamiat", " English", " Urdu", "Arabic"]
+console.log("Solution # 5 " + subjectsName);
+subjectsName.splice(2, 0, "Math", "Science")
+console.log("Solution # 5.2 " + subjectsName);
+
+// Solution # 6
+let cityName = ["Lahore", " Quetta", " Karachi", "Islamabad", "Multan"]
+console.log("Solution # 6 " + cityName);
+cityName.splice(1, 2)
+console.log("Solution # 6.2 " + cityName);
+    
+// Solution # 7
+let countryName = ["Paistan", " Bangladash", " Afghanistan", "Iran", "UAE"]
+console.log("Solution # 7 " + countryName);
+let countryNameN = countryName.slice(0, 3)
+console.log("Solution # 7.2 " + countryNameN);
+
+// Solution # 8
+let fruitS = "apple,banana,mango"
+let fruitSplit = fruitS.split(",")
+console.log("Solution # 8 " + fruitSplit);
+        // exp:            // split(splitter: { [Symbol.split](string: string, limit?: number): string[]; }, limit?: number): string[]
+
+// Solution # 9
+let arrayC = ['J','S']
+let arrayJoin = arrayC.join('')
+console.log("Solution # 9 " + arrayJoin);
+
+// Solution # 10 
+let array1 = ['pen','pencil']
+let array2 = ['eraser','sharpener']
+let concatR = array1.concat(array2)
+console.log("Solution # 10 " + concatR);
+
+// Solution # 11
+// Solution # 12
+// Solution # 13
+// Solution # 14
+// Solution # 15
+// Solution # 16
+// Solution # 17
+// Solution # 18
+// Solution # 19
+// Solution # 20
